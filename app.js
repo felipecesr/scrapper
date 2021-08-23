2cfd1f97e716a680e194a7e37c9b1b78c05723ad
Felipe Cesar <felipecesr@gmail.com>
2021-08-23 12:09:42 -0300

    add app

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-function sayHello() {
-  console.log("Hello");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const crawler = require("crawler-request");
+const sgMail = require("@sendgrid/mail");
+
+console.log(process.env.SENDGRID_API_KEY);
+
+async function sayHello() {
+  async function getDownloadLinkByUrl(url) {
+    const html = await fetch(url).then((res) => res.text());
+    const $ = cheerio.load(html);
+    let name = $(".download").attr("onclick");
+    name = name.match(/location\.href\s*=\s*['"]([^'"]*)['"]/);
+    let link = url + name[1];
+    return link;
+  }
+
+  const url = "https://books.goalkicker.com/DotNETFrameworkBook/";
+  const fileLink = await getDownloadLinkByUrl(url);
+  const fileText = await crawler(fileLink).then((res) => res.text);
+
+  if (!!fileText.match(/akshay anand/i)) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: "felipecesr@gmail.com",
+      from: "jeanfelipecc@gmail.com",
+      subject: "Baixe seu novo arquivo",
+      text: fileLink,
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("mail sent successfully...");
+      })
+      .catch((err) => {
+        console.log(err?.response?.body?.errors);
+      });
+  } else {
+    console.log("palavras chave nao encontradas");
+  }
 }
 sayHello();
